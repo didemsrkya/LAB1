746a2e227ef320201d252703563846c530f67593
didemsrkya <152925587+didemsrkya@users.noreply.github.com>
2025-02-20 15:27:59 +0300

    Create myFirstLabScript.py

---
+++ myFirstLabScript.py
@@ -0,0 +1,42 @@
+#1
+print("Question 1")
+name = input("What is your name?\n")
+print("Hello",name,".")
+studentID = input("What is your Student ID?\n")
+print("Your ID is",studentID,".")
+print("\n")
+
+#2
+print("Question 2")
+var1 = float(input("Enter first number: "))
+var2 = float(input("Enter second number: "))
+
+sum = var1 + var2
+diff = var1 - var2
+prod = var1 * var2
+
+print("var1:",var1,"var2:",var2)
+print("Sum:",sum)
+print("Difference:",diff)
+print("Product:",prod)
+print("\n")
+
+#3
+print("Question 3")
+name = input("Name: ")
+lab = float(input("Lab: "))
+midterm = float(input("Midterm: "))
+final = float(input("Final: "))
+
+lastScore = (lab * 0.25) + (midterm * 0.35) + (final * 0.40)
+
+print("Name:",name)
+print("Lab:",lab)
+print("Midterm:",midterm)
+print("Final:",final)
+print("Last Score:",lastScore)
+print("\n")
+
+#4
+print("Question 4")
+print("*\n**\n***\n**\n*")
